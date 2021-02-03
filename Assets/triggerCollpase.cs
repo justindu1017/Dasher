a565eb0258b8e582206063db1e55537e7325048f
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2021-02-04 03:13:08 +0800

    airwall on one side
    added airwall on one side

---
+++ Assets/triggerCollpase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerCollpase : MonoBehaviour
+{ 
+    public Rigidbody rigidBody;
+    public Rigidbody rigidBody1;
+    public Rigidbody rigidBody2;
+    public Rigidbody rigidBody3;
+
+
+    private void OnTriggerEnter()
+    {
+        rigidBody.useGravity = true;
+        Invoke("Rigidbody1Fall", 0.5f);
+        Invoke("Rigidbody2Fall", 0.8f);
+        Invoke("Rigidbody3Fall", 1.1f);
+        //rigidBody2.useGravity = true;
+        //rigidBody3.useGravity = true;
+    }
+
+
+    private void Rigidbody1Fall() {
+        rigidBody1.useGravity = true;
+    }
+    private void Rigidbody2Fall()
+    {
+        rigidBody2.useGravity = true;
+    }
+    private void Rigidbody3Fall()
+    {
+        rigidBody3.useGravity = true;
+    }
+}
